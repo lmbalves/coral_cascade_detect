c2a32e8c219647b9d80c51b8dd3a62c511e0741a
lmbalves <lmbalves@gmail.com>
2021-01-18 22:00:27 +0000

    added new omp settings

--- cascade_face_detect.cpp
+++ cascade_face_detect.cpp
@@ -81,7 +81,7 @@ void detectAndDisplay( Mat frame )
     {
         face_cascade.detectMultiScale( frame_gray, faces );
     
-        #pragma omp for schedule (dynamic, chunk)
+        #pragma omp for schedule (static)
 
         for ( size_t i = 0; i < faces.size(); i++ )
         {
