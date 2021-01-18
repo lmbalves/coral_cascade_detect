a8301550460c647d01a09e2872b669f28c62dbaa
lmbalves <lmbalves@gmail.com>
2021-01-18 22:41:01 +0000

    added new omp settings

--- cascade_face_detect.cpp
+++ cascade_face_detect.cpp
@@ -77,27 +77,33 @@ void detectAndDisplay( Mat frame )
     std::vector<Rect> faces;
     omp_set_dynamic(2);
     omp_set_num_threads(3);
-    #pragma omp parallel
+    #pragma omp parallel shared(faces, frame_gray, faceROI, eyes)
     {
-        face_cascade.detectMultiScale( frame_gray, faces );
-    
-        #pragma omp for schedule (dynamic)
-
-        for ( size_t i = 0; i < faces.size(); i++ )
+        #pragma omp sections
         {
-            Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-            ellipse( frame, center, Size( faces[i].width/1.5, faces[i].height ), 0, 0, 360, Scalar( 255, 0, 0 ), 4 );
-            Mat faceROI = frame_gray( faces[i] );
-            //-- In each face, detect eyes
-            std::vector<Rect> eyes;
-            #pragma omp parallel
-            eyes_cascade.detectMultiScale( faceROI, eyes );
-            //#pragma omp for
-            for ( size_t j = 0; j < eyes.size(); j++ )
+            #pragma omp section 
+                face_cascade.detectMultiScale( frame_gray, faces );
+    
+            #pragma omp section
             {
-                Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-                int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-                circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
+
+                for ( size_t i = 0; i < faces.size(); i++ )
+                {
+                    Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+                    ellipse( frame, center, Size( faces[i].width/1.5, faces[i].height ), 0, 0, 360, Scalar( 255, 0, 0 ), 4 );
+                    Mat faceROI = frame_gray( faces[i] );
+                    //-- In each face, detect eyes
+                    std::vector<Rect> eyes;
+            
+                    eyes_cascade.detectMultiScale( faceROI, eyes );
+                    
+                    for ( size_t j = 0; j < eyes.size(); j++ )
+                    {
+                        Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+                        int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+                        circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
+                    }
+                }
             }
         }
     }
