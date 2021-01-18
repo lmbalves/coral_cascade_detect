dfd415f52e1a2c9cc78f229ea06bda2d2c7ddc0c
lmbalves <lmbalves@gmail.com>
2021-01-18 19:27:17 +0000

    added new omp settings

--- cascade_face_detect.cpp
+++ cascade_face_detect.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace cv;
 void detectAndDisplay( Mat frame );
-omp_set_dynamic(4);
+
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 int main( int argc, const char** argv )
@@ -78,7 +78,7 @@ void detectAndDisplay( Mat frame )
     {
     face_cascade.detectMultiScale( frame_gray, faces );
     
-    #pragma omp for
+    #pragma omp for schedule(dynamic)
     for ( size_t i = 0; i < faces.size(); i++ )
     {
         Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
