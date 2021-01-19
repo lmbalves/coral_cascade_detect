aca6b579f6eb669dd241ad2359b1390fa4d72ce4
lmbalves <lmbalves@gmail.com>
2021-01-19 00:16:05 +0000

    major makeover

--- cascade_face_detect.cpp
+++ cascade_face_detect.cpp
@@ -88,24 +88,8 @@ void detectAndDisplay( Mat frame )
             }
             #pragma omp section
             {                    
-                //-- detect eyes
                 eyes_cascade.detectMultiScale( frame_gray, eyes );
-            }
-
-            for ( size_t j = 0; j < eyes.size(); j++ )
-            {
-                Point eye_center( eyes[j].x + eyes[j].width/2, eyes[j].y + eyes[j].height/2 );
-                int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-                circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4 );
-            }
-            for ( size_t i = 0; i < faces.size(); i++ )
-            {
-                Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-                ellipse( frame, center, Size( faces[i].width/1.5, faces[i].height ), 0, 0, 360, Scalar( 255, 0, 0 ), 4 );
-            }
-        //-- Show what you got
- 
-                            
+            }               
         }
     }
     for ( size_t j = 0; j < eyes.size(); j++ )
