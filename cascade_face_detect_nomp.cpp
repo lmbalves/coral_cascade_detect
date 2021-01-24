9a1c9b08908c8df58efb3dfa877f6d1d311f4773
lmbalves <lmbalves@gmail.com>
2021-01-24 17:43:14 +0000

    test

--- cascade_face_detect_nomp.cpp
+++ cascade_face_detect_nomp.cpp
@@ -4,6 +4,7 @@
 #include "opencv2/videoio.hpp"
 #include <iostream>
 #include <chrono>
+
 using namespace std;
 using namespace cv;
 void detectAndDisplay( Mat frame );
@@ -11,13 +12,14 @@ CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 int main( int argc, const char** argv )
 {
-    String face_cascade_name = "haarcascade_frontalface_alt.xml";
+
+    String face_cascade_name = "haarcascade_frontalface.xml";
     //-- 1. Load the cascades
     if( !face_cascade.load( face_cascade_name ) )
     {
         cout << "--(!)Error loading face cascade\n";
         return -1;
-    };
+    }
     int camera_device = 0;
     VideoCapture capture;
     //-- 2. Read the video stream
@@ -30,30 +32,32 @@ int main( int argc, const char** argv )
     Mat frame;
     while ( capture.read(frame) )
     {
+
         if( frame.empty() )
         {
             cout << "--(!) No captured frame -- Break!\n";
             break;
         }
         //-- 3. Apply the classifier to the frame
-        
+        #pragma omp single
+        {
         detectAndDisplay( frame );
+        }
 
         if( waitKey(10) == 27 )
         {
             break; // escape
         }
+    
     }
     return 0;
 }
 void detectAndDisplay( Mat frame )
 {
-    
     Mat frame_gray;
 
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
-
     
     //-- Detect faces
     std::vector<Rect> faces;
