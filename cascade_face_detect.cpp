8fe9326de65b6cefef605db01e46c8a850e4b911
lmbalves <lmbalves@gmail.com>
2021-01-25 12:07:40 +0000

    not working?

--- cascade_face_detect.cpp
+++ cascade_face_detect.cpp
@@ -1,7 +1,8 @@
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
+
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
 #include <iostream>
 #include <chrono>
 #include <omp.h>
